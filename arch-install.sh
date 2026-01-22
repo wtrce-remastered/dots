7372a9e9dff209e23a84e003f07bf273f431ae00
wtrce remastered <georgejornd@gmail.com>
2026-01-22 06:29:31 +0300

    flahub fix; fd to packages

--- arch-install.sh
+++ arch-install.sh
@@ -111,17 +111,13 @@ else
     git clone "$GIT_NVIM_REPO" "$NVIM_CONFIG_DIR"
 fi
 
-# installing zen
-
-curl -fsSL https://github.com/zen-browser/updates-server/raw/refs/heads/main/install.sh | sh
-
 # tealdeer db update
 
 tldr --update
 
 # flathub
 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo --user
 
 EOF
 
